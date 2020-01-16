c7d0a03cdda487dc19dc97c93f8c2182d22c66be
Nick Ficano <nficano@gmail.com>
2020-01-16 08:14:26 -0500

    Create README.

--- yakutils/__init__.py
+++ yakutils/__init__.py
@@ -1,3 +1,9 @@
 # flake8: noqa
+__title__ = 'yakutils'
+__version__ = '1.0.0'
+__author__ = 'Nick Ficano'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2020 Nick Ficano'
+
 from .utils import read_csv
 from .utils import read_json

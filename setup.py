86f71e1bb2683ec2525a57c9f202da5ab98a6790
Nick Ficano <nficano@gmail.com>
2020-01-22 22:32:18 -0500

    Bump version: 1.0.8 → 1.0.9

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class UploadCommand(Command):
 
 setup(
     name="yakutils",
-    version="1.0.8",
+    version="1.0.9",
     author="Nick Ficano",
     author_email="nficano@gmail.com",
     packages=["yakutils"],

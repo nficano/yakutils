5e485943aa0a00b4635234832f8ca69acfcff1fc
Nick Ficano <nficano@gmail.com>
2020-01-26 11:57:20 -0500

    No conflict module names

--- yakutils/__init__.py
+++ yakutils/__init__.py
@@ -5,20 +5,20 @@ __author__ = "Nick Ficano"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2020 Nick Ficano"
 
-from ._csv import iter_csv
-from ._csv import read_csv
-from ._json import iter_json
-from ._json import read_json
-from ._json import JSONEncoder
-from ._logging import SANE_BASE_LOGGING_CONFIG
-from ._logging import setup_logging
-from ._datetime import to_timezone
-from ._datetime import (
+from .csv_ import iter_csv
+from .csv_ import read_csv
+from .json_ import iter_json
+from .json_ import read_json
+from .json_ import JSONEncoder
+from .logging_ import SANE_BASE_LOGGING_CONFIG
+from .logging_ import setup_logging
+from .datetime_ import to_timezone
+from .datetime_ import (
     iso8601_to_datetime,
     datetime_to_iso8601,
     date_to_iso8601,
     datetime_to_unixtimestamp,
 )
-from ._random import random_string
-from ._hashlib import md5
-from ._hashlib import sha1
+from .random_ import random_string
+from .hashlib_ import md5
+from .hashlib_ import sha1

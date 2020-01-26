88d375bd5eed7fe7dc129c5fea72c16c83f20775
Nick Ficano <nficano@gmail.com>
2020-01-26 12:00:51 -0500

    Bump version: 1.1.2 → 1.1.3

--- yakutils/__init__.py
+++ yakutils/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 __title__ = "yakutils"
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "Nick Ficano"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2020 Nick Ficano"

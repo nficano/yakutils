a4485a8a68af7de8e7a183b9f3524aed1499afc9
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-01-27 18:41:58 -0800

    Make _hashlib more generic
    This adds support for hashing arbitrary objects, and it also allows the user to use hash_any to hash any object by specifying a hash algorithm or passing in a _hashlib.HASH object (eg. the user creates hashlib.sha1())

--- yakutils/_hashlib.py
+++ yakutils/_hashlib.py
@@ -1,24 +1,49 @@
 """This module contains boilerplate hashlib helpers."""
 import hashlib
 
-__all__ = ["md5", "sha1", "sha384", "sha3_384"]
+__all__ = ["md5", "sha1", "sha384", "sha3_384", "hash_any"]
 
 
-def md5(s):
-    """Compute the MD5 of a given string."""
-    return hashlib.md5(s.encode("utf-8")).hexdigest()
+def md5(obj):
+    """Compute the MD5 of the given object."""
+    if isinstance(obj, str):
+        return hashlib.md5(obj.encode("utf-8")).hexdigest()
+    return hashlib.md5(bytes(obj)).hexdigest()
 
 
-def sha1(s):
-    """Compute the SHA1 of a given string."""
-    return hashlib.sha1(s.encode("utf-8")).hexdigest()
+def sha1(obj):
+    """Compute the SHA1 of the given object."""
+    if isinstance(obj, str):
+        return hashlib.sha1(obj.encode("utf-8")).hexdigest()
+    return hashlib.sha1(bytes(obj)).hexdigest()
 
 
-def sha384(s):
-    """Compute the SHA384 of a given string."""
-    return hashlib.sha384(s.encode("utf-8")).hexdigest()
+def sha384(obj):
+    """Compute the SHA384 of the given object."""
+    if isinstance(obj, str):
+        return hashlib.sha384(obj.encode("utf-8")).hexdigest()
+    return hashlib.sha384(bytes(obj)).hexdigest()
 
 
-def sha3_384(s):
-    """Compute the SHA3 384 of a given string."""
-    return hashlib.sha3_384(s.encode("utf-8")).hexdigest()
+def sha3_384(obj):
+    """Compute the SHA3 384 of the given object."""
+    if isinstance(obj, str):
+        return hashlib.sha3_384(obj.encode("utf-8")).hexdigest()
+    return hashlib.sha3_384(bytes(obj)).hexdigest()
+
+
+def hash_any(obj, hasher):
+    """ If hasher is a string, return the hash of the obj as specified by hasher. If hasher is an instance of hashlib's
+     hashers, return the hash of the object from hasher. """
+    if isinstance(obj, str):
+        obj_bytes = obj.encode("utf-8")
+    else:
+        obj_bytes = bytes(obj)
+
+    if isinstance(hasher, str):
+        return hashlib.new(hasher, obj_bytes).hexdigest()
+    elif str(type(hasher)) == "<class '_hashlib.HASH'>":
+        hasher.update(obj_bytes)
+        return hasher.hexdigest()
+    else:
+        raise TypeError("hasher is not a ??")

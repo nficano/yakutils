4b1d2142726cfeac832140cb87594cd84564e696
Nick Ficano <nficano@gmail.com>
2020-01-28 08:06:56 -0500

    Consistancy updates

--- stubs/yakutils/__init__.pyi
+++ stubs/yakutils/__init__.pyi
@@ -3,5 +3,5 @@ from ._datetime import date_to_iso8601 as date_to_iso8601, datetime_to_iso8601 a
 from ._hashlib import md5 as md5, sha1 as sha1, sha384 as sha384, sha3_384 as sha3_384
 from ._json import iter_json as iter_json, json_defaults as json_defaults, read_json as read_json
 from ._logging import SANE_BASE_LOGGING_CONFIG as SANE_BASE_LOGGING_CONFIG, setup_logging as setup_logging
-from ._random import random_string as random_string
+from ._random import random_string as random_string, random_tolerance as random_tolerance
 from ._urllib import update_qs as update_qs

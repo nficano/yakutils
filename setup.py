7620df8603014f1270564a12e28d7026581bb253
Nick Ficano <nficano@gmail.com>
2020-02-06 09:47:25 -0500

    Bump version: 1.6.0 → 1.7.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class UploadCommand(Command):
 
 setup(
     name="yakutils",
-    version="1.6.0",
+    version="1.7.0",
     author="Nick Ficano",
     author_email="nficano@gmail.com",
     packages=["yakutils"],
--- yakutils/__init__.py
+++ yakutils/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 __title__ = "yakutils"
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 __author__ = "Nick Ficano"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2020 Nick Ficano"
